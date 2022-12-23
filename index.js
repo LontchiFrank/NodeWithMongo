30057820bcd84df111fc8b0b0ccd73ff33919341
LontchiFrank <fodjof6@gmail.com>
2022-12-23 20:29:35 +0100

    connected to database

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 require("dotenv").config();
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const PORT = process.env.PORT || 4000;
 
--- model/post_model.js
+++ model/post_model.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    unique: true,
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Post", postSchema);
