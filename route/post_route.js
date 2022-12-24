791936c245bc8a552ada0720a1192c738ce085a2
LontchiFrank <fodjof6@gmail.com>
2022-12-24 18:58:41 +0100

    all the route for crud created

--- route/post_route.js
+++ route/post_route.js
@@ -24,4 +24,31 @@ router.post("/new", async (req, res) => {
   }
 });
 
+//update the post
+router.put("/:id", async (req, res) => {
+  try {
+    const updatePost = await Post.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatePost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Delete the post
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletePost = await Post.findByIdAndDelete(req.params.id);
+    res
+      .status(200)
+      .json({ message: "Post has been deleted successfully", deletePost });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
